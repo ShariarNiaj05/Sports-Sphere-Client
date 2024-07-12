5dc4e068a8f1fb77a198249fe5ccd60735ca28f0
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-12 21:53:32 +0600

    all product page initial setup

--- src/pages/AllProduct.tsx
+++ src/pages/AllProduct.tsx
@@ -1,5 +1,22 @@
+import Title from "@/components/custom/Title";
+import Container from "@/components/shared/Container";
+
 const AllProduct = () => {
-  return <div>all-product</div>;
+  return (
+    <Container>
+      <div className="mt-20">
+        <Title>All Product</Title>
+        <div className=" mt-20 flex flex-col gap-10">
+          {/* filter section  */}
+
+          <div>filter</div>
+
+          {/* All Product section  */}
+          <div>all AllProduct</div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default AllProduct;
