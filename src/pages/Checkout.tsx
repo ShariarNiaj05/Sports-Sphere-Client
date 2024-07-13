3b53ea923a9974970c90416d052a630c4e85d610
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-14 01:35:48 +0600

    checkout page implemented

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,5 +1,54 @@
+import Title from "@/components/custom/Title";
+import Container from "@/components/shared/Container";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const Checkout = () => {
-  return <div>Checkout.tsx</div>;
+  return (
+    <Container>
+      <div className="my-28">
+        <Title>Check Out</Title>
+
+        <div className=" mt-12 flex flex-col md:flex-row gap-8">
+          <div className="basis-3/4 space-y-8">
+            {/* Full Name  */}
+            <Input
+              type="text"
+              placeholder="Full Name."
+              className="lg:w-[70%]"
+              // value={searchTerm}
+              // onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <Input
+              type="email"
+              placeholder="Email."
+              className="lg:w-[70%]"
+              // value={searchTerm}
+              // onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <Input
+              type="text"
+              placeholder="Phone Number."
+              className="lg:w-[70%]"
+              // value={searchTerm}
+              // onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <Input
+              type="text"
+              placeholder="Address"
+              className="lg:w-[70%]"
+              // value={searchTerm}
+              // onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+          <div className="basis-1/4 flex flex-col gap-3">
+            <Button>Cash On Delivery</Button>
+            <Button className="bg-secondary">Stripe </Button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Checkout;
