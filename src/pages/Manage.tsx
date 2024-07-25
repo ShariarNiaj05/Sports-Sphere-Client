52327f8b2c6a3d4075be3ac17c8860267d3aa215
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-25 20:22:41 +0600

    tab added to manage product page

--- src/pages/Manage.tsx
+++ src/pages/Manage.tsx
@@ -1,5 +1,87 @@
+import Title from "@/components/custom/Title";
+import Container from "@/components/shared/Container";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const Manage = () => {
-  return <div>Manage.jsx</div>;
+  return (
+    <Container>
+      <div className="mt-20">
+        <Title>Manage Product</Title>
+        <div className="mt-20 flex flex-col gap-10  px-5">
+          <Tabs defaultValue="productList" className="w-[400px]">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="productList">Product List</TabsTrigger>
+              <TabsTrigger value="addProduct">Add Product</TabsTrigger>
+            </TabsList>
+
+            {/* Product List tab  */}
+            <TabsContent value="productList">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Account</CardTitle>
+                  <CardDescription>
+                    Make changes to your account here. Click save when you're
+                    done.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="space-y-1">
+                    {/* <Label htmlFor="name">Name</Label> */}
+                    <Input id="name" defaultValue="Pedro Duarte" />
+                  </div>
+                  <div className="space-y-1">
+                    {/* <Label htmlFor="username">Username</Label> */}
+                    <Input id="username" defaultValue="@peduarte" />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save changes</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+
+            {/* Add Product Tab */}
+            <TabsContent value="addProduct">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Password</CardTitle>
+                  <CardDescription>
+                    Change your password here. After saving, you'll be logged
+                    out.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="space-y-1">
+                    {/* <Label htmlFor="current">Current password</Label> */}
+                    <Input id="current" type="password" />
+                  </div>
+                  <div className="space-y-1">
+                    {/* <Label htmlFor="new">New password</Label> */}
+                    <Input id="new" type="password" />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save password</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Manage;
