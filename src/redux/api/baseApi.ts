6a52e686737b812d6e564106f2bc74ac4564728d
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-28 00:26:01 +0600

    deployed

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,8 +2,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
-const baseUrl = "http://localhost:5000/api/v1";
-// const baseUrl = "https://sports-sphere-server-swart.vercel.app/api/v1";
+// const baseUrl = "http://localhost:5000/api/v1";
+const baseUrl = "https://sports-sphere-server-swart.vercel.app/api/v1";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
