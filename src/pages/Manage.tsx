d7bef752b36fe639d8430258636688bf3faaca5e
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-30 23:51:30 +0600

    removed unused import

--- src/pages/Manage.tsx
+++ src/pages/Manage.tsx
@@ -2,19 +2,6 @@ import AddProduct from "@/components/custom/AddProduct";
 import ProductListTable from "@/components/custom/ProductListTable";
 import Title from "@/components/custom/Title";
 import Container from "@/components/shared/Container";
-
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-// import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Manage = () => {
