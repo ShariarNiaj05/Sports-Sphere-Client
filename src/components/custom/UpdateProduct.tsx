5f5795d2e3242425d29cbae34f3f498b3defd0ac
ShariarNiaj05 <shariarn85@gmail.com>
2024-07-31 00:29:52 +0600

    update product file changes

--- src/components/custom/UpdateProduct.tsx
+++ src/components/custom/UpdateProduct.tsx
@@ -3,8 +3,9 @@ import { Button } from "../ui/button";
 import { Card, CardContent, CardFooter } from "../ui/card";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { useAddProductMutation } from "@/redux/api/baseApi";
 import { toast } from "sonner";
+import Container from "../shared/Container";
+import Title from "./Title";
 
 const UpdateProduct = () => {
   const [productName, setProductName] = useState<string>("");
@@ -15,8 +16,6 @@ const UpdateProduct = () => {
   const [productDescription, setProductDescription] = useState<string>("");
   const [price, setPrice] = useState<number>(100);
   const [image, setImage] = useState<string>("");
-
-  const [addProduct] = useAddProductMutation();
 
   const handleForm = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -32,7 +31,10 @@ const UpdateProduct = () => {
       image,
     };
     console.log("payload:", payload);
-    const result = await addProduct(payload);
+
+    //   update logic
+    // const result = await addProduct(payload);
+    const result = {};
     console.log(result);
     if (result?.error) {
       toast.error("Failed to add product");
@@ -41,102 +43,109 @@ const UpdateProduct = () => {
   };
 
   return (
-    <form onSubmit={handleForm}>
-      <Card>
-        <CardContent className="space-y-2">
-          {/* product name and sports category  */}
-          <div className="flex gap-2 w-full mt-2">
-            <div className="w-1/2">
-              <Label htmlFor="current">Product Name</Label>
-              <Input
-                id="productName"
-                type="text"
-                value={productName}
-                onChange={(e) => setProductName(e.target.value)}
-              />
-            </div>
-            <div className="w-1/2">
-              <Label htmlFor="new">Sports Category</Label>
-              <Input
-                id="sportsCategory"
-                type="text"
-                value={sportsCategory}
-                onChange={(e) => setSportsCategory(e.target.value)}
-              />
-            </div>
-          </div>
+    <Container>
+      <div className="my-28">
+        <Title>Update Product</Title>
+        <form className="mt-28" onSubmit={handleForm}>
+          <Card>
+            <CardContent className="space-y-2">
+              {/* product name and sports category  */}
+              <div className="flex gap-2 w-full mt-2">
+                <div className="w-1/2">
+                  <Label htmlFor="current">Product Name</Label>
+                  <Input
+                    id="productName"
+                    type="text"
+                    value={productName}
+                    onChange={(e) => setProductName(e.target.value)}
+                  />
+                </div>
+                <div className="w-1/2">
+                  <Label htmlFor="new">Sports Category</Label>
+                  <Input
+                    id="sportsCategory"
+                    type="text"
+                    value={sportsCategory}
+                    onChange={(e) => setSportsCategory(e.target.value)}
+                  />
+                </div>
+              </div>
 
-          {/* stock quantity and brand  */}
-          <div className="flex gap-2 w-full mt-2">
-            <div className="w-1/2">
-              <Label htmlFor="new">Stock Quantity</Label>
-              <Input
-                id="stockQuantity"
-                type="number"
-                value={stockQuantity}
-                onChange={(e) => setStockQuantity(parseFloat(e.target.value))}
-              />
-            </div>
-            <div className="w-1/2">
-              <Label htmlFor="current">brand</Label>
-              <Input
-                id="brand"
-                type="text"
-                value={brand}
-                onChange={(e) => setBrand(e.target.value)}
-              />
-            </div>
-          </div>
+              {/* stock quantity and brand  */}
+              <div className="flex gap-2 w-full mt-2">
+                <div className="w-1/2">
+                  <Label htmlFor="new">Stock Quantity</Label>
+                  <Input
+                    id="stockQuantity"
+                    type="number"
+                    value={stockQuantity}
+                    onChange={(e) =>
+                      setStockQuantity(parseFloat(e.target.value))
+                    }
+                  />
+                </div>
+                <div className="w-1/2">
+                  <Label htmlFor="current">brand</Label>
+                  <Input
+                    id="brand"
+                    type="text"
+                    value={brand}
+                    onChange={(e) => setBrand(e.target.value)}
+                  />
+                </div>
+              </div>
 
-          {/* rating and product description  */}
-          <div className="flex gap-2 w-full mt-2">
-            <div className="w-1/2">
-              <Label htmlFor="new">Rating</Label>
-              <Input
-                id="rating"
-                type="number"
-                value={rating}
-                onChange={(e) => setRating(parseFloat(e.target.value))}
-              />
-            </div>
-            <div className="w-1/2">
-              <Label htmlFor="current">Product Description</Label>
-              <Input
-                id="productDescription"
-                type="text"
-                value={productDescription}
-                onChange={(e) => setProductDescription(e.target.value)}
-              />
-            </div>
-          </div>
+              {/* rating and product description  */}
+              <div className="flex gap-2 w-full mt-2">
+                <div className="w-1/2">
+                  <Label htmlFor="new">Rating</Label>
+                  <Input
+                    id="rating"
+                    type="number"
+                    value={rating}
+                    onChange={(e) => setRating(parseFloat(e.target.value))}
+                  />
+                </div>
+                <div className="w-1/2">
+                  <Label htmlFor="current">Product Description</Label>
+                  <Input
+                    id="productDescription"
+                    type="text"
+                    value={productDescription}
+                    onChange={(e) => setProductDescription(e.target.value)}
+                  />
+                </div>
+              </div>
 
-          {/* price and image url  */}
-          <div className="flex gap-2 w-full mt-2">
-            <div className="w-1/2">
-              <Label htmlFor="new">Price ($)</Label>
-              <Input
-                id="price"
-                type="number"
-                value={price}
-                onChange={(e) => setPrice(parseFloat(e.target.value))}
-              />
-            </div>
-            <div className="w-1/2">
-              <Label htmlFor="current">Image URL</Label>
-              <Input
-                id="image"
-                type="text"
-                value={image}
-                onChange={(e) => setImage(e.target.value)}
-              />
-            </div>
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button>Update Product</Button>
-        </CardFooter>
-      </Card>
-    </form>
+              {/* price and image url  */}
+              <div className="flex gap-2 w-full mt-2">
+                <div className="w-1/2">
+                  <Label htmlFor="new">Price ($)</Label>
+                  <Input
+                    id="price"
+                    type="number"
+                    value={price}
+                    onChange={(e) => setPrice(parseFloat(e.target.value))}
+                  />
+                </div>
+                <div className="w-1/2">
+                  <Label htmlFor="current">Image URL</Label>
+                  <Input
+                    id="image"
+                    type="text"
+                    value={image}
+                    onChange={(e) => setImage(e.target.value)}
+                  />
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Update Product</Button>
+            </CardFooter>
+          </Card>
+        </form>
+      </div>
+    </Container>
   );
 };
 
