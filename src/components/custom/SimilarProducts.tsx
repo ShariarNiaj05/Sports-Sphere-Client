4de1bd238fc7e6446aa78f123023ed86cba6e884
ShariarNiaj05 <shariarn85@gmail.com>
2024-08-11 00:45:26 +0600

    title added for similar product in product details page

--- src/components/custom/SimilarProducts.tsx
+++ src/components/custom/SimilarProducts.tsx
@@ -1,5 +1,16 @@
+import Container from "../shared/Container";
+import Title from "./Title";
+
 const SimilarProducts = () => {
-  return <div>Similar Products</div>;
+  return (
+    <Container>
+      <div className="mt-20">
+        <Title>More from this category</Title>
+      </div>
+
+      <div>6 similar product here</div>
+    </Container>
+  );
 };
 
 export default SimilarProducts;
