ceb3247afdcd97f33be16f557aebe5ea24c64403
ShariarNiaj05 <shariarn85@gmail.com>
2024-12-15 18:52:33 +0600

    UI: designing product carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@ import Categories from "@/components/custom/Categories";
 import Contact from "@/components/custom/Contact";
 import Featured from "@/components/custom/Featured";
 import HeroSection from "@/components/custom/HeroSection";
+import ProductCarousel from "@/components/custom/ProductCarousel";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <Featured />
+      <ProductCarousel />
       <Categories />
       <Contact />
     </div>
